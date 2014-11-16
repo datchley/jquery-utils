bd37ee7d0814e97f89eaa89718c6bcf73658900a
David Atchley <david.atchley@answers.com>
2014-11-15 21:39:07 -0600

    minor updates

--- src/string.jquery.js
+++ src/string.jquery.js
@@ -52,9 +52,49 @@
     String.prototype.escapeHtml = function() {
         return escapeHtml(this);
     };
-    
-    // Assign global functions
-    this.escapeRegExp = escapeRegExp;
-    this.escapeHtml = escapeHtml;
- 
+
+    /**
+     * parseUrl -  parse the current window url or a passed in url and
+     * return an object that breaks the url down into its constituent parts,
+     * including giving access to any query params
+     * @param url=window.location.href {string} - the url to parse
+     * @return  {object} - object with parts of url accessible via property keys
+     */
+    function parseUrl(url) {
+        var _url = (url || window.location.href).match(/^(?:(https?:)\/\/)?([^\?\#\/\:]+)(?::(\d+))?([^\?\#]+)?(\?[^#]+)?(#[^\s]*)?$/),
+            params = _url[5] || false;
+
+        if (!_url.length) {
+            // couldn't parse url string
+            return false;
+        }
+
+        if (params && !/^$/.test(params)) {
+            var pairs = params.replace(/^\?/,'').split('&');
+            params = {};
+            // console.log("pairs=%o", pairs);
+            for (var i=0, l=pairs.length; i<l; i++) {
+                var keyval = pairs[i].split('=');
+                params[keyval[0]] = keyval[1];
+            }
+        }
+        return {
+            source: url,
+            protocol: _url[1],
+            host: _url[2],
+            port: _url[3],
+            path: _url[4],
+            query: _url[5],
+            hash: _url[6],
+            params: params
+        };
+    }
+
+
+    // Extend jQuery with utility methods
+    $.extend({
+        escapeHtml: escapeHtml,
+        escapeRegExp: escapeRegExp,
+        parseUrl: parseUrl
+    });
 }));
--- test/string.spec.js
+++ test/string.spec.js
@@ -2,13 +2,114 @@
 describe("String Utilities", function() {
 
     var chars = ["&","<",">",'"',"'","/"],
-        escaped = ["&amp;","&lt;","&gt;",'&quot;','&#39;','&#x2F;'];
+        escaped = ["&amp;","&lt;","&gt;",'&quot;','&#39;','&#x2F;'],
+        // test URLs to parse
+        urls = [
+            "https://www.site.com",
+            "http://local.www.site.com",
+            "https://www.site.com:8080",
+            "http://www.site.com/path/to/file",
+            "http://www.site.com:8080/path/to/file",
+            "https://www.site.com/path/to/file?no-cache=1&error=1",
+            "http://www.site.com:8080/path/to/file#slide1",
+            "https://www.site.com/path/to/file?no-cache=1&error=1#slide1",
+        ],
+        // Expected parsed URL results from above urls
+        parsed_urls = [
+            {   
+                hash: undefined,
+                host: "www.site.com",
+                params: false,
+                path: undefined,
+                port: undefined,
+                protocol: "https:",
+                query: undefined,
+                source: "https://www.site.com"
+            },
+            {   
+                hash: undefined,
+                host: "local.www.site.com",
+                params: false,
+                path: undefined,
+                port: undefined,
+                protocol: "http:",
+                query: undefined,
+                source: "http://local.www.site.com"
+            },
+            {   
+                hash: undefined,
+                host: "www.site.com",
+                params: false,
+                path: undefined,
+                port: "8080",
+                protocol: "https:",
+                query: undefined,
+                source: "https://www.site.com:8080"
+            },
+            {   
+                hash: undefined,
+                host: "www.site.com",
+                params: false,
+                path: "/path/to/file",
+                port: undefined,
+                protocol: "http:",
+                query: undefined,
+                source: "http://www.site.com/path/to/file"
+            },
+            {   
+                hash: undefined,
+                host: "www.site.com",
+                params: false,
+                path: "/path/to/file",
+                port: "8080",
+                protocol: "http:",
+                query: undefined,
+                source: "http://www.site.com:8080/path/to/file"
+            },
+            {   
+                hash: undefined,
+                host: "www.site.com",
+                params: {
+                    "no-cache": "1",
+                    "error": "1"
+                },
+                path: "/path/to/file",
+                port: undefined,
+                protocol: "https:",
+                query: "?no-cache=1&error=1",
+                source: "https://www.site.com/path/to/file?no-cache=1&error=1"
+            },
+            {   
+                hash: "#slide1",
+                host: "www.site.com",
+                params: false,
+                path: "/path/to/file",
+                port: "8080",
+                protocol: "http:",
+                query: undefined,
+                source: "http://www.site.com:8080/path/to/file#slide1"
+            },
+            {   
+                hash: "#slide1",
+                host: "www.site.com",
+                params: {
+                    "no-cache": "1",
+                    "error": "1"
+                },
+                path: "/path/to/file",
+                port: undefined,
+                protocol: "https:",
+                query: "?no-cache=1&error=1",
+                source: "https://www.site.com/path/to/file?no-cache=1&error=1#slide1"
+            }
+        ];
 
     jasmine.getEnv().addReporter(new jasmine.JSReporter2());
 
-    it("provide a global set of functions", function() {
-        expect(window.escapeRegExp).toBeDefined();
-        expect(window.escapeHtml).toBeDefined();
+    it("provides methods on jQuery object", function() {
+        expect($.escapeRegExp).toBeDefined();
+        expect($.escapeHtml).toBeDefined();
+        expect($.parseUrl).toBeDefined();
     });
     it("provide methods on native String object", function() {
         expect(String.prototype.escapeRegExp).toBeDefined();
@@ -18,14 +119,21 @@ describe("String Utilities", function() {
         var start = chars.join(''),
             result = escaped.join('');
 
-        expect(window.escapeHtml(start)).toEqual(result);
+        expect($.escapeHtml(start)).toEqual(result);
         expect(start.escapeHtml()).toEqual(result);
     });
     it("escapeRegExp() properly esacpes meta characters", function() {
         var meta =  "/-[]{}()*+?.,^$",
             meta_escaped = "\\/\\-\\[\\]\\{\\}\\(\\)\\*\\+\\?\\.\\,\\^\\$";
 
-        expect(window.escapeRegExp(meta)).toEqual(meta_escaped);
+        expect($.escapeRegExp(meta)).toEqual(meta_escaped);
         expect(meta.escapeRegExp()).toEqual(meta_escaped);
+    });
+    it("parseUrl() properly parses url strings", function() {
+        for (var i=0, l=urls.length; i<l; i++) {
+            var parsed = $.parseUrl(urls[i]),
+                expected = parsed_urls[i];
+            expect(parsed).toEqual(expected);
+        }
     });
 });
